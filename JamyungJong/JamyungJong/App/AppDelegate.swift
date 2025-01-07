5f83e8a4a5c5d958fd71ee74fd165df83c0f61e0
jwl-98 <apple_komputer@icloud.com>
2025-01-07 19:48:26 +0900

    first commit

---
+++ JamyungJong/JamyungJong/App/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  JamyungJong
+//
+//  Created by 진욱의 Macintosh on 1/7/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
+
---
+++ JamyungJong/JamyungJong/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JamyungJong
+//
+//  Created by 진욱의 Macintosh on 1/7/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
